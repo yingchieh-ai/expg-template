1c0f3ae1c5d1bae3f769a6bdddec0f48f27ce0ad
Samuel Wang <chieh0919@gmail.com>
2026-05-03 14:31:18 +0000

    feat: add cookie-parser middleware
    Integrate cookie-parser to enable cookie parsing in the Express application.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
-import express, { type Express } from 'express';
 import cors from '@/middlewares/cors';
+import cookieParser from 'cookie-parser';
+import express, { type Express } from 'express';
 
 const app: Express = express();
 
 app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
